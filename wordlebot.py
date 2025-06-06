294ccd6d97e0dd15354e8826de0e594c70025579
csch <cschlisner@gmail.com>
2025-06-05 19:11:44 -0600

    combine source dict and frequency dict. Weight guesses on letter freq instead of word freq for many possibile answers.

--- wordlebot.py
+++ wordlebot.py
@@ -1,24 +1,29 @@
-INCORRECT_LETTERS = ""
-CORRECT_LETTERS = ""
-CORRECT_PLACEMENT = ['','','','','']
-INCORRECT_PLACEMENT = [[],[],[],[],[]]
+INCORRECT_LETTERS = "esirclokhn"
+CORRECT_LETTERS = "atu"
+CORRECT_PLACEMENT = ['','a','','','']
+INCORRECT_PLACEMENT = [['a'],[''],['u'],['a'],['t']]
 
 ############################################################
-
-
-import itertools
 import json
 import os
-import csv
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-freq = {}
-with open('unigrams_freq_5let.csv') as f:
-    r = csv.reader(f)
-    for row in r:
-        freq[row[0]] = int(row[1])
+# import csv
+# freq = {}
+# with open('unigrams_freq_5let.csv', 'r') as f:
+#     r = csv.reader(f)
+#     for row in r:
+#         freq[row[0]] = int(row[1])
+# sol_freq = {}
+# with open('wordle_possibles.csv', 'r') as f:
+#     wp = csv.reader(f)
+#     for w in wp:
+#         sol_freq[w[0]] = int(freq.get(w[0],9000))
+# with open('solution_freq.json', 'w') as f:
+#     json.dump(sol_freq, f)
+# exit()
 
-with open('dictionary_5let.json', 'r') as f:
+with open('solution_freq.json', 'r') as f:
     dictionary = json.load(f)
 
 letter_freq = {
@@ -81,12 +86,14 @@ for p in dictionary.keys():
 print()
 
 print("possible solutions:")
-list.sort(solutions, key=lambda w: freq.get(w, -100000000), reverse=True)
+list.sort(solutions, key=lambda w: dictionary[w], reverse=True)
 print(solutions)
 
 if (len(solutions)>1):
     unknown_letters = list(dict.fromkeys(''.join(solutions)))
     unknown_letters = [s for s in unknown_letters if s not in CORRECT_LETTERS]
+    if len(solutions) > 10:
+        list.sort(unknown_letters, key=lambda l: letter_freq[l], reverse=True)
     print(f"\nunknown letters: %s"%unknown_letters)
     def generate_guesses(words = solutions):
         guess_set = set()
@@ -97,10 +104,13 @@ if (len(solutions)>1):
             for c in INCORRECT_LETTERS:
                 if c in w:
                     count=-100
-            if words != solutions:
-                for c in CORRECT_LETTERS:
-                    if c in w:
-                        count=-100
+            for c in CORRECT_LETTERS:
+                if c in w:
+                    if words != solutions:
+                        count += -100
+                    else: 
+                        count += 1
+                        weight += len(unknown_letters)
             for i, c in enumerate(unknown_letters):
                 if c in w:
                     count += 1
@@ -125,6 +135,6 @@ if (len(solutions)>1):
         return [g[2] for g in best_guesses]
     
     print(f"\nTo attempt solve you should guess one of: %s"%generate_guesses())
-    print(f"\nTo narrow solutions you should guess one of: %s"%generate_guesses(freq.keys()))
+    print(f"\nTo narrow solutions you should guess one of: %s"%generate_guesses(dictionary.keys()))
 
 
